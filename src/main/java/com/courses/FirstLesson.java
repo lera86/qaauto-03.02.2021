126f6f688f54f08e38c147475a2e8bb52858b10b
lera86 <v.ulyanova@hotmail.com>
2021-03-06 20:15:38 +0300

    код урок2

--- src/main/java/com/courses/FirstLesson.java
+++ src/main/java/com/courses/FirstLesson.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import java.util.List;
 
 public class FirstLesson {
 
@@ -13,7 +14,7 @@ public class FirstLesson {
       WebDriverManager.chromedriver().setup();
        WebDriver driver = new ChromeDriver();
         driver.get("http://saucedemo.com");
-        WebElement usernameField = driver.findElement(By.className("form-input"));
+        WebElement usernameField = driver.findElement(By.className("form_input"));
         WebElement passwordField = driver.findElement(By.xpath("//input[2]"));
         WebElement loginButton = driver.findElement(By.xpath("//input[@type='submit']"));
         usernameField.sendKeys("standard_user");
@@ -24,7 +25,18 @@ public class FirstLesson {
         WebElement addToCart = list.get(2);
         addToCart.click();
 
+        WebElement filter = driver.findElement(By.className("product_sort_container"));
+        Select select = new Select(filter);
+
+        List<WebElement> listOfOptions = select.getOptions();
+
+       for(int i = 0; i < listOfOptions.size(); i++) {
+        System.out.println(listOfOptions.get(i).getText());
+    }
+     select.selectByValue("lohi");
+     select.selectByVisibleText("Price (high to low)");
+
+
 
     }
-
 }
