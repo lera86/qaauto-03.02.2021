d00965587fd02260173dbd405fa87df1fa546bc0
valeria ulyanova <v.ulyanova@hotmail.com>
2021-03-10 19:35:58 +0300

    lesson2 code

--- src/main/java/com/courses/FirstLesson.java
+++ src/main/java/com/courses/FirstLesson.java
@@ -11,18 +11,22 @@ import java.util.List;
 public class FirstLesson {
 
     public static void main(String[] args) {
+     int indexForReversionFilter = 1;
+
       WebDriverManager.chromedriver().setup();
        WebDriver driver = new ChromeDriver();
         driver.get("http://saucedemo.com");
+
         WebElement usernameField = driver.findElement(By.className("form_input"));
         WebElement passwordField = driver.findElement(By.xpath("//input[2]"));
         WebElement loginButton = driver.findElement(By.xpath("//input[@type='submit']"));
+
         usernameField.sendKeys("standard_user");
         passwordField.sendKeys("secret_sauce");
         loginButton.click();
 
         List<WebElement> list = driver.findElements(By.xpath("//button"));
-        WebElement addToCart = list.get(2);
+        WebElement addToCart = list.get(3);
         addToCart.click();
 
         WebElement filter = driver.findElement(By.className("product_sort_container"));
@@ -33,10 +37,20 @@ public class FirstLesson {
        for(int i = 0; i < listOfOptions.size(); i++) {
         System.out.println(listOfOptions.get(i).getText());
     }
-     select.selectByValue("lohi");
-     select.selectByVisibleText("Price (high to low)");
 
+       select.selectByIndex(indexForReversionFilter);
 
+        List<WebElement> listNew = driver.findElements(By.xpath("//button"));
+        WebElement addToCart3 = listNew.get(4);
+        addToCart3.click();
+
+        WebElement burgerButton = driver.findElement(By.id("react-burger-menu-btn"));
+
+        WebElement logoutButton = driver.findElement(By.id("logout_sidebar_link"));
+        burgerButton.click();
+        logoutButton.click();
+
+        driver.quit();
 
     }
 }
